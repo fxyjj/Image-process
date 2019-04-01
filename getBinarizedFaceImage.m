8fcf3a5473afc5341ca40c6838067c3724571b57
zy22077 <zy22077@nottingham.edu.cn>
2019-04-01 21:42:25 +0800

    2019.04.01 night submition about the implementations of the filters in task2

--- getBinarizedFaceImage.m
+++ getBinarizedFaceImage.m
@@ -10,19 +10,23 @@ S = testImg_hsv(:,:,2);
 V = testImg_hsv(:,:,3);
 for i = 1 : x
     for j = 1 : y
-          if(C(1,1) <= H(i,j) && H(i,j) <= C(1,2) && C(1,3)<=S(i,j) && C(1,4)>= S(i,j))
+          if(H(i,j) <= C(1,2)/10000 && S(i,j)>=C(1,3)/10000 && S(i,j)<=C(1,4)/10000)
+             img2(i,j) = 255;
+             %testImg_hsv(i,j,3) = 255;
+          end
+           if(H(i,j) <= C(2,2)/10000 && S(i,j)>=C(2,3)/10000 && S(i,j)<=C(2,4)/10000)
              img2(i,j) = 255;
              %testImg_hsv(i,j,3) = 255;
           end
     end
 end
 figure;
-subplot(2,2,1);
-imshow(img);
-subplot(2,2,2);
+%subplot(2,2,1);
 imshow(img2);
-subplot(2,2,3);
-imshow(testImage);
+% subplot(2,2,2);
+% imshow(img2);
+% subplot(2,2,3);
+% imshow(testImage);
 %img = testImg_hsv(:,:,3);
 
 
--- task2.m
+++ task2.m
@@ -5,13 +5,12 @@ for i = 1 : 3
     Mean_filter(i,2) = 1/9;
     Mean_filter(i,3) = 1/9;
 end
-
 % 5 x 5 Gaussian filter with ∂ = 1
-Guassian_filter = fspecial('gaussian',5,1);
-
+gaussian_filter = G_Filter(1,3);
+img = conv2(I,gaussian_filter,'same');
 % 3 x 3 median filter
-
+img = meidianFilter(I,3);
 % 3 x 3 anisotropic filter with similarity function of (D - d) / D
-
+img = Anisotropic(I,3);
 % 5 x 5 bilaterial filter with ∂ = 1
-function img = BF(I,sizef)
+img = BF(I,5);
