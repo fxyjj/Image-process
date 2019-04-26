d6476912c42ffba86785c69412ad64680087ebc4
zy22077 <zy22077@nottingham.edu.cn>
2019-04-26 21:38:20 +0800

    "more clearer, 2019.04.26"

--- Otsu.m
+++ Otsu.m
@@ -1,14 +1,17 @@
 %Otsu thresholding
  ori = rgb2gray(imread('retina_images_01_10/1.tif'));
-k=graythresh(Ivessel);              %得到最优阈值
-J=imbinarize(Ivessel,0.028);
-  BW2 = edge(ori,'Canny');
-  figure;
-  imshow(BW2);
+k=graythresh(res1);              %得到最优阈值
+J=imbinarize(res1,k);
+j = connection_comp(J,5);
+%   BW2 = edge(ori,'Canny');
+%   figure;
+%   imshow(BW2);
+  
 figure;
-imshow(J+BW2);
-figure;
- edge = [0.001,0.001:0.001:0.1,0.1];
- h = histogram(Ivessel,edge);
+imshow(j);
+
+% figure;
+%  edge = [0.001,0.001:0.001:0.1,0.1];
+%  h = histogram(Ivessel,edge);
 %figure;
 %imshow(dilation(dilation(J)));
--- testH.m
+++ testH.m
@@ -11,9 +11,35 @@ for i = 1 : r
         end
     end
 end
+res = im2uint8( mat2gray( log(1 + double(Ivessel)) ) );
+  res1 = im2uint8( mat2gray( log(1 + double(res)) ) );
+  Mean_filter = zeros(3,3);
+for i = 1 : 3
+    Mean_filter(i,1) = 1/9;
+    Mean_filter(i,2) = 1/9;
+    Mean_filter(i,3) = 1/9;
+end
+%  res_g =  BF(res,3,10,10);
+  
+  % gamma correction 
+  l_img = imadjust(res,[],[],0.5);
+   ll_img = imadjust(res,[],[],1);
+  res_g =  BF(ll_img,3,1,1);
+  res_A = Anisotropic(ll_img,3);
+   res_M = conv2(ll_img,Mean_filter,'same');
+  
+%   s_img = realpow(f_img,1/gamma);
+  
+ 
 figure;  
 imshow(uint8(I));
 title('原灰度图');
 figure;
-imshow(Ivessel);
+imshow(res_M);
 title('Frangi滤波结果');
+figure;
+imshow(l_img);
+title('more clear,more noisy');
+figure;  
+imshow(ll_img);
+title('Gamma图');
